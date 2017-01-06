f314b4f4c6a044bd32801ee9b133d90f95f549cd
Wisit Srimala <Wisit Srimala>
2017-01-06 22:48:44 +0700

    edit product ux/ui

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -2,52 +2,53 @@
 @section('content')
 <div class="w3-container">
 	<br><br>
-	<div class="w3-row">
-	    <div class="w3-col" style="width:20%"><p></p></div>
-	    <div class="w3-col" style="width:60%">
-	    	@if(Auth::user()->level != 'user')
-	    			<h4><a href="{{URL::asset('/product/add')}}" class="w3-btn w3-round w3-green" role="button">New Product</a></h4>
-	    	@endif
-	    	<table class="w3-table w3-bordered w3-card-4 w3-animate-opacity">
-	    		<thead>
-	    			<tr class="w3-teal">
-	    				<th width="10%">Code</th>
-						<th width="20%">Product Name</th>
-						<th width="60%">Amount</th>
-						@if(Auth::user()->level != 'user')
-							<th width="5%">Edit</th>
-							<th width="5%">Delete</th>
-						@endif
-	    			</tr>
-	    		</thead>
-	    		<tbody>
-	    			@foreach ($product as $s )
-	    				<tr>
-		    				<td>{{$s->id}}</td>
-							<td>{{$s->name}}</td>
-							<td>
-								<div class="w3-progress-container w3-round-xlarge">
-									@if($s->amount >= $s->amount_alert)
-										<div id="myBar" class="w3-progressbar w3-green w3-round-xlarge" style="width:{{($s->amount/$s->amount_max)*100}}%">
-											&nbsp&nbsp{{$s->amount}}/{{$s->amount_max}}
-										</div>
+	<div class="w3-row w3-responsive">
+			<div class="w3-col m2 s2" style="width:20%"><p></p></div>
+			<div class="w3-col m8 s8" style="width:60%">
+				<br>
+				<h3 class="w3-text-blue">บริหารจัดการถังน้ำ</h3>
+				<br>
+				<a href="{{URL::asset('/product/add')}}" class="w3-btn w3-border w3-round-large w3-blue w3-right">เพิ่มสินค้า</a>
+				<br><br>
+				<table class="w3-table w3-bordered w3-hoverable w3-margin-top w3-centered">
+					<thead>
+					  	<tr class="w3-blue">
+						    <th style="width:10%">Code</th>
+						    <th style="width:20%">Product Name</th>
+						    <th>Quantity</th>
+						    @if(Auth::user()->level != 'user')
+							<th style="width:10%">Edit</th>
+						    <th style="width:10%">Delete</th>
+							@endif
+					  	</tr>
+				  	</thead>
+				  	<tbody>
+				  		@foreach ($product as $s )
+					  	<tr>
+					      	<td>P{{ sprintf('%04u', $s->id) }}</td>
+					      	<td>{{ $s->name }}</td>
+					      	<td>
+					      		<div class="w3-progress-container">
+								    @if($s->amount >= $s->amount_alert)
+									<div id="myBar" class="w3-center w3-text-white w3-green w3-round-xlarge" style="width:{{($s->amount/$s->amount_max)*100}}%">
+										&nbsp&nbsp{{$s->amount}}/{{$s->amount_max}}
+									</div>
 									@else
-										<div id="myBar" class="w3-progressbar w3-red w3-round-xlarge" style="width:{{($s->amount/$s->amount_max)*100}}%">
+										<div id="myBar" class="w3-center w3-text-white w3-red w3-round-xlarge" style="width:{{($s->amount/$s->amount_max)*100}}%">
 											&nbsp&nbsp{{$s->amount}}/{{$s->amount_max}}
 										</div>
 									@endif
-								</div><br>
-							</td>
-							@if(Auth::user()->level != 'user')
-								<td><a href="{{URL::asset('/product/edit/')}}/{{$s->id}}"><span class="fa fa-edit"></span></a></td>
-								<td><a href="{{URL::asset('/product/delete/')}}/{{$s->id}}"><span class="fa fa-trash"></span></a></td>
-							@endif
-						</tr>
-					@endforeach
-	    		</tbody>
-	    	</table>
-	    </div>
-	    <div class="w3-col" style="width:20%"><p></p></div>
-	</div>
+								</div>
+					      	</td>
+							<td><a href="{{URL::asset('/product/edit/')}}/{{$s->id}}"><img src="img/edit.png" style="width:20px"></a></td>
+					      	<td><a href="{{URL::asset('/product/delete/')}}/{{$s->id}}"><img src="img/delete.png" style="width:20px"></a></td>
+					    </tr>
+					   	@endforeach
+				    </tbody>
+				  </table>				
+				  <br>
+			</div>
+			<div class="w3-col m2 s2" style="width:20%"><p></p></div>
+		</div>
 </div>
 @endsection
